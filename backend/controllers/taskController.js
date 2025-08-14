dfd72359aba9fd1c4717f2176d47a06a2194adcb
Bobbye <n9566929@qut.edu.au>
2025-08-15 02:37:11 +1000

    changes taskController.js and add test folder

--- backend/controllers/taskController.js
+++ backend/controllers/taskController.js
@@ -38,7 +38,7 @@ res.status(500).json({ message: error.message });
 }
 };
 
-const deleteTask = async(res,res)=>{
+const deleteTask = async(req,res)=>{
 try {
 const task = await Task.findById(req.params.id);
 if (!task) return res.status(404).json({ message: 'Task not found' });
@@ -48,3 +48,5 @@ res.json({ message: 'Task deleted' });
 res.status(500).json({ message: error.message });
 }
 };
+
+module.exports = { getTasks, addTask, updateTask, deleteTask };
